47ad7d3f3034f62d09a2f7b3c228e5bbbe905aa5
Vladlen <vladlen@vozhzhaev.ru>
2023-03-17 22:31:18 +0300

    add User class

--- src/server/Server.java
+++ src/server/Server.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 public class Server {
     public static void main(String[] args) {
         ArrayList<Socket> sockets = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
         // 0.0.0.0 - 255.255.255.255 NAT
         try {
             ServerSocket serverSocket = new ServerSocket(9123);
@@ -17,24 +18,27 @@ public class Server {
             while (true){
                 Socket socket = serverSocket.accept();
                 System.out.println("Клиент подключился");
-                sockets.add(socket);
+                User user = new User(socket);
+                users.add(user);
                 Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            DataInputStream in = new DataInputStream(socket.getInputStream());
+                            user.getOut().writeUTF("Введите имя: ");
+                            String name = user.getIn().readUTF();
+                            user.setName(name);
+                            user.getOut().writeUTF(user.getName()+" добро пожаловать на сервер!");
                             String clientMessage;
                             while (true){
-                                clientMessage = in.readUTF();
+                                clientMessage = user.getIn().readUTF();
                                 System.out.println(clientMessage);
-                                for (Socket socket : sockets) {
-                                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                                    out.writeUTF(clientMessage.toUpperCase());
+                                for (User user1 : users) {
+                                    user1.getOut().writeUTF(user.getName()+": "+clientMessage);
                                 }
                             }
                         }catch (IOException e){
                             System.out.println("Клиент отключился");
-                            sockets.remove(socket);
+                            users.remove(user);
                         }
                     }
                 });
